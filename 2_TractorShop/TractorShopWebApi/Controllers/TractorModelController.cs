d6fc16262530e8e3ff25dd33216ff8054c890b00
arumbocic <arumbocic@gmail.com>
2022-03-24 19:16:49 +0100

    get and post

--- 2_TractorShop/TractorShopWebApi/Controllers/TractorModelController.cs
+++ 2_TractorShop/TractorShopWebApi/Controllers/TractorModelController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -10,90 +12,183 @@ namespace TractorShopWebApi.Controllers
 {
     public class TractorModelController : ApiController
     {
-        static List<TractorModel> machines = new List<TractorModel>()
-        {
-            new TractorModel ("tractor", "John Deere"),
-            new TractorModel ("tractor", "CaseIH"),
-            new TractorModel ("tractor", "New Holland")
-        };
+        static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TractorShopDB;Integrated Security=True";
 
         // GET api/values
         [HttpGet]
         [Route("tractormodel/getall")]
         public HttpResponseMessage GetAll()
         {
-            if (machines == null || machines.Count == 0)
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            using (connection)
             {
-                return Request.CreateResponse(HttpStatusCode.NotFound, "The list is empty!");
+                SqlCommand command = new SqlCommand("SELECT * FROM TractorModel;", connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<TractorModel> tractorModels = new List<TractorModel>();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        TractorModel tractorModel = new TractorModel();
+
+                        tractorModel.Id = reader.GetInt32(0);
+                        tractorModel.Model = reader.GetString(1);
+                        tractorModel.Code = reader.GetGuid(2);
+                        tractorModel.BrandId = reader.GetInt32(3);
+
+                        tractorModels.Add(tractorModel);
+                    }
+
+                    return Request.CreateResponse(HttpStatusCode.OK, tractorModels);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "The list is empty!");
+                }
+                reader.Close();
+                connection.Close();
             }
-            return Request.CreateResponse(HttpStatusCode.OK, machines);
         }
 
         // GET api/values/5
+        //TODO: Postavi stupac Model na Unique values
         [HttpGet]
         [Route("tractormodel/get/{id}")]
-        public HttpResponseMessage GetById(Guid id)
+        public HttpResponseMessage GetById(int Id)
         {
-            var response = machines.Find(r => r.Id == id);
+            SqlConnection connection = new SqlConnection(connectionString);
 
-            if (response == null)
+            using (connection)
             {
-                return Request.CreateResponse(HttpStatusCode.NotFound, $"There is no element with id {id}!");
+                SqlCommand command = new SqlCommand($"SELECT * FROM TractorModel WHERE Id = '{Id}';", connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<TractorModel> tractorModels = new List<TractorModel>();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        TractorModel tractorModel = new TractorModel();
+
+                        tractorModel.Id = reader.GetInt32(0);
+                        tractorModel.Model = reader.GetString(1);
+                        tractorModel.Code = reader.GetGuid(2);
+                        tractorModel.BrandId = reader.GetInt32(3);
+
+                        tractorModels.Add(tractorModel);
+                    }
+
+                    return Request.CreateResponse(HttpStatusCode.OK, tractorModels);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "The list is empty!");
+                }
+                reader.Close();
+                connection.Close();
             }
-            return Request.CreateResponse(HttpStatusCode.OK, response);
         }
+
 
         // POST api/values
         [HttpPost]
         [Route("tractormodel/set")]
-        public HttpResponseMessage Post(TractorModel machine)
+        public HttpResponseMessage Post(TractorModel postModel)
         {
-            if (machine == null)
+            SqlConnection conn = new SqlConnection(connectionString);
+            using (conn)
             {
-                return Request.CreateResponse(HttpStatusCode.BadRequest, "Provided an empty object!");
+                if (postModel != null)
+                {
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    string sqlQuery = "INSERT INTO TractorModel (Model, BrandId) VALUES (@Model, @BrandId);";
+                    da.InsertCommand = new SqlCommand(sqlQuery, conn);
+
+                    conn.Open();
+
+                    da.InsertCommand.Parameters.Add("@Model", SqlDbType.NVarChar).Value = postModel.Model;
+                    da.InsertCommand.Parameters.Add("@BrandId", SqlDbType.Int).Value = postModel.BrandId;
+                    da.InsertCommand.ExecuteNonQuery();
+
+                    conn.Close();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Provided an empty object!");
+                }
             }
-            machines.Add(machine);
-            return Request.CreateResponse(HttpStatusCode.Created, machines);
         }
+
 
         // PUT api/values/5
         [HttpPut]
         [Route("tractormodel/update/{id}")]
-        public HttpResponseMessage PutById(Guid id, TractorModel machine)
+        public HttpResponseMessage UpdateById(int Id, TractorModel updateModel)
         {
-            var item = machines.Find(r => r.Id == id);
-
-            item.Id = machine.Id;
-            item.Type = machine.Type;
-            item.Brand = machine.Brand;
-
-            if (item == null)
+            SqlConnection conn = new SqlConnection(connectionString);
+            using (conn)
             {
-                return Request.CreateResponse(HttpStatusCode.NotFound, $"There is no element with id {id}!");
+                if (updateModel != null)
+                {
+                    conn.Open();
+
+                    string sqlQuery1 = $"SELECT Id FROM TractorModel;";
+                    SqlCommand command = new SqlCommand(sqlQuery1, conn);
+                    SqlDataReader dr = command.ExecuteReader();
+
+
+
+
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    string sqlQuery2 = $"UPDATE TABLE TractorModel SET Model = @Model, BrandId = @BrandId WHERE Id = '{updateModel.Id}';";
+                    da.InsertCommand = new SqlCommand(sqlQuery2, conn);
+                    da.InsertCommand.Parameters.Add("@Model", SqlDbType.NVarChar).Value = updateModel.Model;
+                    da.InsertCommand.Parameters.Add("@BrandId", SqlDbType.Int).Value = updateModel.BrandId;
+                    da.InsertCommand.ExecuteNonQuery();
+
+                    conn.Close();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, $"There is no element with id {updateModel.Id}!");
+                }
             }
-            return Request.CreateResponse(HttpStatusCode.OK, machines);
         }
 
-        // DELETE api/values/5
-        [HttpDelete]
-        [Route("tractormodel/delete/{id}")]
-        public HttpResponseMessage DeleteById(Guid id)
-        {
+        //// DELETE api/values/5
+        //[HttpDelete]
+        //[Route("tractormodel/delete/{id}")]
+        //public HttpResponseMessage DeleteById(Guid id)
+        //{
 
-            if (id == null)
-            {
-                return Request.CreateResponse(HttpStatusCode.BadRequest, "Provided an non existing id!");
-            }
+        //    if (id == null)
+        //    {
+        //        return Request.CreateResponse(HttpStatusCode.BadRequest, "Provided an non existing id!");
+        //    }
 
-            var item = machines.Find(r => r.Id == id);
-            machines.Remove(item);
+        //    var item = machines.Find(r => r.Id == id);
+        //    machines.Remove(item);
 
-            if (item == null)
-            {
-                return Request.CreateResponse(HttpStatusCode.NotFound, $"There is no element with id {id}!");
-            }
-            return Request.CreateResponse(HttpStatusCode.OK, machines);
+        //    if (item == null)
+        //    {
+        //        return Request.CreateResponse(HttpStatusCode.NotFound, $"There is no element with id {id}!");
+        //    }
+        //    return Request.CreateResponse(HttpStatusCode.OK, machines);
 
-        }
+        //}
     }
 }
--- 2_TractorShop/TractorShopWebApi/Models/TractorModel.cs
+++ 2_TractorShop/TractorShopWebApi/Models/TractorModel.cs
@@ -4,22 +4,9 @@ namespace TractorShopWebApi.Models
 {
     public class TractorModel
     {
-        public Guid Id { get; set; }
-        public string Type { get; set; }
-        public string Brand { get; set; }
-
-        public TractorModel(string type, string brand)
-        {
-            Id = Guid.NewGuid();
-            Type = type;
-            Brand = brand;
-        }
-
-        public TractorModel(Guid id, string type, string brand)
-        {
-            Id = id;
-            Type = type;
-            Brand = brand;
-        }
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public Guid Code { get; set; }
+        public int BrandId { get; set; }
     }
 }
