015f443ea0b226e8eaac30f593b7dba0c2460f50
arumbocic <arumbocic@gmail.com>
2022-03-30 22:30:10 +0200

    adding FilterTractorModel

---
+++ 4_TractorShop/TractorModel.Common/FilterTractorModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TractorModel.Common
+{
+    public class FilterTractorModel : IFilterTractorModel
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int BrandId { get; set; }
+
+        public FilterTractorModel(int id, string model, int brandId)
+        {
+            Id = id;
+            Model = model;
+            BrandId = brandId;
+        }
+    }
+}
---
+++ 4_TractorShop/TractorModel.Common/IFilterTractorModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TractorModel.Common
+{
+    public interface IFilterTractorModel
+    {
+        int Id { get; set; }
+        string Model { get; set; }
+        int BrandId { get; set; }
+    }
+}
